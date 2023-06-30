a8b202b6d93570ba7d95feaad7c00a8b70f7a92b
Josh Wong <josdanwong@gmail.com>
2023-06-29 20:47:08 -0400

    route to bring up aboutus page

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -7,6 +7,7 @@ const isAuthenticated = require('../../middleware/isAuthenticated');
 router.get('/', (req, res) => res.render('homepage'));
 router.get('/register', (req, res) => res.render('register'));
 router.get('/login', (req, res) => res.render('login'));
+router.get('/aboutus', (req, res) => res.render('aboutus'))
 
 // Pages with data
 router.get('/dashboard', isAuthenticated, PageController.getDashboard);

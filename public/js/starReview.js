e88195461772ec5b3c180a758b0639311e976aae
Josh Wong <josdanwong@gmail.com>
2023-06-29 20:11:51 -0400

    added js file to handle logic for users selecting stars and updating their corresponding color

---
+++ public/js/starReview.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const ratingInputs = doucment.querySelectorAll('input[name="rating"]')
+    const starLabels = document.querySelector('label[for^="star"]')
+
+    starLables.forEach((label, index) => {
+        label.addEventListener('click', async () => {
+            const rating = ratingInputs[index].value; 
+
+        for (let i = 0; i <= index; i++){
+            starLabels[i].classList.add('selected');
+        }
+
+        for (let i = index + 1; i < starLabels.length; i++){
+            starLabels[i].classList.remove('selected');
+        }
+
+        const ratingNumber = {
+            rating: rating
+        }
+
+        try {
+            const response = await fetch ('/reviews', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(ratingNumber)
+            })
+            if (response.ok) {
+                console.log('Rating submitted');
+            } else{
+                console.log('Error entering rating', response.status)
+            }
+        } catch (error) {
+            console.log('Error entering rating', error)
+        }
+        });
+    });
+});
